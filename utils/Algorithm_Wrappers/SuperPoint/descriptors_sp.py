f1db27b701ccdd103d87176c28f537bb668b668f
ufukefe <32823854+ufukefe@users.noreply.github.com>
2021-12-06 11:53:01 +0300

    Add files via upload

---
+++ utils/Algorithm_Wrappers/SuperPoint/descriptors_sp.py
@@ -0,0 +1,13 @@
+        if do_match:
+            # Perform the matching.
+            pred = matching({'image0': inp0, 'image1': inp1})
+            pred = {k: v[0].cpu().numpy() for k, v in pred.items()}
+            kpts0, kpts1 = pred['keypoints0'], pred['keypoints1']
+            matches, conf = pred['matches0'], pred['matching_scores0']
+            dsc0, dsc1 = pred['descriptors0'], pred['descriptors1']
+            timer.update('matcher')
+
+            # Write the matches to disk.
+            out_matches = {'keypoints0': kpts0, 'keypoints1': kpts1,
+                           'matches': matches, 'match_confidence': conf,
+                           'descriptors0': dsc0, 'descriptors1': dsc1}
